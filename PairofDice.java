e6cb3551ea2a3bc8cab3e4e6aef11d918c446a9e
SamHahn01 <45635108+SamHahn01@users.noreply.github.com>
2018-12-07 11:18:51 -0600

    Add files via upload
    Die.java is the class that rolls the dice, and PairofDice.java is the class that shows what rolls.

---
+++ PairofDice.java
@@ -0,0 +1,34 @@
+/**
+ * @(#)PairofDice.java
+ * @author Sam Hahn
+ * Demonstrates the use of a class with overloaded constructors
+ * @version 1.00 2018/12/7
+ */
+
+
+public class PairofDice 
+{
+	//Creates two die objects, then rolls both dice a set number of times
+	//counting the number of snake eyes that occur.
+    public static void main (String[]args) 
+    {
+    	final int ROLLS = 1000;
+    	int boxCars = 0, num1, num2;
+    	
+    	Die die1 = new Die(); //creates a six-sided die
+    	Die die2 = new Die(); 
+    	
+    	for (int roll = 1; roll <= ROLLS; roll++)
+    	{
+    		num1 = die1.roll();
+    		num2 = die2.roll();
+    		
+    	if (num1 == 6 && num2 == 6) //check for box carsww
+    		boxCars++;
+    	}
+    	
+    	System.out.println ("Number of rolls: " + ROLLS);
+    	System.out.println ("Number of box cars: " + boxCars);
+    	System.out.println ("Ratio: " + (double)boxCars/ROLLS);
+    }
+}
